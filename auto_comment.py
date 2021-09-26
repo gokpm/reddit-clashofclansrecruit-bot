a05ae3b725324a5881e15ffedcd6f7de5414a4f3
Gokul PM <contact@gokulpm.com>
2021-09-26 22:54:12 +0530

    auto_comment asyncpraw

--- auto_comment.py
+++ auto_comment.py
@@ -8,39 +8,43 @@ from readWrite import *
 
 load_dotenv()
 genCache()
-th = ['th12', 'th13', 'th14']
 
-async def autoComment():
+async def initReddit():
     reddit = asyncpraw.Reddit(
     client_id=os.environ['REDDIT_CLIENT_ID'],
     client_secret=os.environ['REDDIT_CLIENT_SECRET'],
     user_agent=os.environ['REDDIT_USER_AGENT'],
-    username=['REDDIT_USERNAME'],
-    password=['REDDIT_PASSWORD'])
+    username=os.environ['REDDIT_USERNAME'],
+    password=os.environ['REDDIT_PASSWORD'])
     reddit.read_only = False
     sub = await reddit.subreddit('clashofclansrecruit')
     posts = [post async for post in sub.new(limit = 250)]
     posts.reverse()
+    return posts
+
+def execWebhook(embed_title, embed_description, embed_color, embed_url):
+    webhook = DiscordWebhook(url=os.environ['WEBHOOK'])
+    embed = DiscordEmbed(title=embed_title, description=embed_description, color = embed_color, url = embed_url)
+    webhook.add_embed(embed)
+    webhook.execute()
+    
+async def autoComment():
+    th = ['th12', 'th13', 'th14']
+    posts = await initReddit()
     comment = readComment()
     latest_post_time = readCache()
     while True:
         for post in posts:
             if post.created_utc > latest_post_time:
                 post_content = post.title + ' ' + post.selftext
-                post_content = post_content.lower.split()
+                post_content = post_content.lower().split()
                 if (('[searching]' in post_content) and (any(item in th for item in post_content))):
                     latest_post_time = post.created_utc
                     writeCache(latest_post_time)
-                    webhook = DiscordWebhook(url=os.environ['WEBHOOK'])
-                    embed = DiscordEmbed(title=post.title, description=post.selftext, color = '800020', url = post.url)
-                    webhook.add_embed(embed)
-                    webhook.execute()
-                    post.reply(comment)
-                    webhook = DiscordWebhook(url=os.environ['WEBHOOK'])
-                    embed = DiscordEmbed(title='Replied', color = '00FFFF', url = post.url)
-                    webhook.add_embed(embed)
-                    webhook.execute()
-                    time.sleep(1000)
+                    execWebhook(post.title, post.selftext, '800020', post.url)
+                    await post.reply(comment)
+                    execWebhook('Replied', '', '00FFFF', post.url)
+                    await asyncio.sleep(1000)
 
 if __name__ == '__main__':
     asyncio.run(autoComment())
