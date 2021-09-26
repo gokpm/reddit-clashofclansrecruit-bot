68a2e719f0fd8f384dc4879a348dfd401d50eb4c
Gokul PM <contact@gokulpm.com>
2021-09-26 21:44:55 +0530

    Initial Commit asyncpraw

--- auto_comment.py
+++ auto_comment.py
@@ -1,101 +1,61 @@
-'''
-Version:            0.1
-Date last modified: 07-09-2021
-Modified by:        icemelting
-Contributed by:     icemelting
-'''
-
-import praw
-from discord_webhook import DiscordWebhook, DiscordEmbed
+import asyncpraw
+import asyncio
+import os
 import time
+from discord_webhook import DiscordWebhook, DiscordEmbed
+from dotenv import load_dotenv
+from readWrite import *
 
-recruit_banner = '''<RECRUITMENT_MESSAGE>'''
+load_dotenv()
+genCache()
+th = ['th12', 'th13', 'th14']
 
-th9_list =  ['th9', 'th10', 'th11', 'th12', 'th13', 'th14']
-th10_list = ['th10', 'th11', 'th12', 'th13', 'th14']
-th11_list = ['th11', 'th12', 'th13', 'th14']
-th12_list = ['th12', 'th13', 'th14']
-th13_list = ['th13', 'th14']
-th14_list = ['th14']
+async def autoComment():
+    reddit = asyncpraw.Reddit(
+    client_id=os.environ['REDDIT_CLIENT_ID'],
+    client_secret=os.environ['REDDIT_CLIENT_SECRET'],
+    user_agent=os.environ['REDDIT_USER_AGENT'],
+    username=['REDDIT_USERNAME'],
+    password=['REDDIT_PASSWORD'])
+    reddit.read_only = False
+    sub = await reddit.subreddit('clashofclansrecruit')
+    posts = [post async for post in sub.new(limit = 250)]
+    posts.reverse()
+    comment = readComment()
+    latest_post_time = readCache()
+    while True:
+        for post in posts:
+            if post.created_utc > latest_post_time:
+                post_content = post.title + ' ' + post.selftext
+                post_content = post_content.lower.split()
+                if (('[searching]' in post_content) and (any(item in th for item in post_content))):
+                    latest_post_time = post.created_utc
+                    writeCache(latest_post_time)
+                    webhook = DiscordWebhook(url=os.environ['WEBHOOK'])
+                    embed = DiscordEmbed(title=post.title, description=post.selftext, color = '800020', url = post.url)
+                    webhook.add_embed(embed)
+                    webhook.execute()
+                    post.reply(comment)
+                    webhook = DiscordWebhook(url=os.environ['WEBHOOK'])
+                    embed = DiscordEmbed(title='Replied', color = '00FFFF', url = post.url)
+                    webhook.add_embed(embed)
+                    webhook.execute()
+                    time.sleep(1000)
 
-def TH9(webhook_url):
-    if any(item in th9_list for item in post_text):
-        execute_webhook(webhook_url)
-            
-def TH10(webhook_url):
-    if any(item in th10_list for item in post_text):
-        execute_webhook(webhook_url)
-            
-def TH11(webhook_url):
-    if any(item in th11_list for item in post_text):
-        execute_webhook(webhook_url)
-            
-def TH12(webhook_url):
-    if any(item in th12_list for item in post_text):
-        execute_webhook(webhook_url)
-            
-def TH13(webhook_url):
-    if any(item in th13_list for item in post_text):
-        execute_webhook(webhook_url)
-            
-def TH14(webhook_url):
-    if any(item in th14_list for item in post_text):
-        execute_webhook(webhook_url)
-            
-def execute_webhook(webhook_url):
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title=post.title, description=post.selftext, color = '800020', url = post.url)
-    webhook.add_embed(embed)
-    webhook.execute()
-    post.reply(recruit_banner)
-    webhook = DiscordWebhook(url=webhook_url)
-    embed = DiscordEmbed(title='Replied', color = '00FFFF', url = post.url)
-    webhook.add_embed(embed)
-    webhook.execute()
-    time.sleep(1000)
-
-subreddit_name = 'clashofclansrecruit'
-clan = {<CLAN_TAG>:<WEBHOOK_URL>}
-
-cache_filepath = <CACHE_FILE_PATH>
-
-cache = open(cache_filepath, 'a+')
-cache.close()
-cache = open(cache_filepath, 'r')
-already_seen = cache.read().split()
-cache.close()
-
-reddit = praw.Reddit(
-    client_id=<CLIENT_ID>,
-    client_secret=<CLIENT_SECRET>,
-    user_agent=<BOT_NAME>,
-    username=<REDDIT_USERNAME>,
-    password=<REDDIT_PASSWORD>,
-)
-reddit.read_only = False
-sub = reddit.subreddit(subreddit_name)
-
-i = 0
-while True:
-    time_start = time.perf_counter()
-    latest = sub.new(limit = 250)
-    for post in latest:
-        if not post.id in already_seen:
-            post_text = post.title + ' ' + post.selftext
-            post_text = post_text.lower().split()
-            if '[searching]' in post_text:
-                cache = open(cache_filepath, 'a+')
-                cache.write(post.id+'\n')           
-                cache.close()
-                already_seen.append(post.id)
-                execute_webhook(clan[<CLAN_TAG>])
-    i += 1
-    time_end = time.perf_counter()
-    time_min = int((time_end - time_start)/60)
-    time_sec = int((time_end - time_start)%60)
-    print(f'Cycle {i} Ping: {time_min}m {time_sec}s')
-
-
+if __name__ == '__main__':
+    asyncio.run(autoComment())
+    
+    
+    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+        
     
     
 
+    
